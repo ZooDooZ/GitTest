9cee5f46121f1db541b84d256811ff4030982627
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-08-25 15:02:43 +0200

    Add new class

--- src/main/java/org/FilipS/App.java
+++ src/main/java/org/FilipS/App.java
@@ -4,5 +4,18 @@ package org.FilipS;
 public class App {
     public static void main( String[] args ) {
 
+        Car audi = new Car();
+        audi.setModel("A3");
+        audi.setColor("bialy");
+        audi.setProductionYear(2010);
+
+        audi.drive(0);
+        System.out.println("This car mileage is: " + audi.getMileage() + " kilometers");
+        System.out.println(audi.getModel());
+        System.out.println(audi.getColor());
+        System.out.println(audi.getProductionYear());
+        System.out.println(audi.isUsed());
+
+
     }
 }
