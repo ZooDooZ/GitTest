c8ccd06c93834145b37414dc07c250db36b6816b
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-09-07 00:35:00 +0200

    The program breaks a string into a word and reduces uppercase to lowercase letters inside a sentence

--- src/main/java/org/FilipS/App.java
+++ src/main/java/org/FilipS/App.java
@@ -1,8 +1,28 @@
 package org.FilipS;
 
 
+import java.util.LinkedList;
+
 public class App {
     public static void main( String[] args ) {
-
+        String str =  "SiemankoZiomekCoUCiebie.JakTwojeZdrowie?";
+        String[] array = convertStrToChar(str);
+        String sentence = wordsSeparator(array, str);
+        System.out.println(sentence);
     }
+
+    public static String[] convertStrToChar(String str){
+        return str.split("");
+    }
+
+    public static String wordsSeparator(String[] array, String str){
+        for(int i = 1; i < array.length; i++){
+            if(Character.isUpperCase(array[i].charAt(0))){
+                str = str.replace(array[i], " " + array[i].toLowerCase());
+            }
+        }
+
+        return str.trim();
+    }
+
 }
