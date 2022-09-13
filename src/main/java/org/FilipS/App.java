29db9097fb052ccd7da014752f3b4fe5b69dbf3f
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-09-14 00:09:12 +0200

    Add methods oneRoundScore, printScoreAfterOneRound, printResultOfTheGame

--- src/main/java/org/FilipS/App.java
+++ src/main/java/org/FilipS/App.java
@@ -1,10 +1,7 @@
 package org.FilipS;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public static void main( String[] args ) {
@@ -18,24 +15,28 @@ public class App {
         // LOGIC LAYER
         // X 1. CREATE BASE WITH QUESTIONS AND ANSWERS
         // ? 2. COMPUTER DRAW QUESTION
-        // 3. COMPARE ANSWER FORM USER AND COMPUTER
+        // X 3. COMPARE ANSWER FORM USER AND COMPUTER
         // 4. ADDITION OR SUBTRACTION POINTS (SCORE)
         // 5. CREATE NEXT ROUNDS
 
         // OUTPUT LAYER
         // X 1. COMPUTER ASK THE PLAYER THE QUESTION
         // X 2. RETURN INFORMATION ABOUT ANSWER
-        // 3. RETURN INFORMATION ABOUT SCORE
-        // 4. PRINT SCORES ABOUT ALL PLAYER AFTER ROUND
+        // X 3. RETURN INFORMATION ABOUT SCORE
+        // X 4. PRINT SCORES ABOUT ALL PLAYER AFTER ROUND
         // 5. PRINT WHO IS WINNER AND HOW MANY POINTS GET EACH PLAYER
+
+        // Dopracować szczegóły jeżeli chodzi remis oraz poprawić kod aby był przejrzysty
+        // Przetestować kod na różne sposoby np dla 2/3/4 graczy i na więcej rund.
+        // DORZUCIĆ PYTAŃ
+        // LOSOWOŚĆ PYTAŃ!??!!??!!??
 
         Integer numberOfPlayers = numberOfPlayers();
         Integer numberOfRounds = numberOfRounds();
         ArrayList<String> playerNicks = getPlayerNicks(numberOfPlayers);
-        HashMap<String, Integer> questionsBase = questionsBase();
-        HashMap<Integer, Integer> numberOfAnswer = getNumberOfAnswer(questionsBase);
-        printInfAboutAnswer(numberOfAnswer);
-
+        int[] playersScore = oneRoundScore(numberOfPlayers, playerNicks, numberOfRounds);
+        printScoreAfterOneRound(playersScore,playerNicks);
+        printResultOfTheGame(playersScore,playerNicks);
 
     }
     public static void printInfoAboutGame(){
@@ -113,18 +114,74 @@ public class App {
             numberOfAnswer = scanner.nextInt();
             Integer numberOfCorrectAnswer = question.getValue();
             mapAnswers.put(numberOfAnswer, numberOfCorrectAnswer);
-            questionsBase.remove(question);
+            questionsBase.remove(question.getKey(),question.getValue());
             break;
         }
         return mapAnswers;
     }
 
-    public static void printInfAboutAnswer(HashMap<Integer, Integer> mapAnswer){
+    public static Integer printInfAboutAnswer(HashMap<Integer,Integer> mapAnswer){
+        Integer playerPoints = 0;
         for(Map.Entry<Integer, Integer> number : mapAnswer.entrySet()){
             if(number.getKey().equals(number.getValue())){
-                System.out.println("Correct answer!");
+                System.out.println("Correct answer! You get 3 points!");
+                playerPoints += 3;
             } else {
-                System.out.println("Wrong answer!");
+                System.out.println("Wrong answer! You lose 1 point!");
+                playerPoints -= 1;
+            }
+        }
+        return playerPoints;
+    }
+// CO Z TĄ METODA??? CZY ROBIĆ PODZIAŁ NA POJEDYŃCZE METODY BO TA WYŻEJ METODA WYKONUJE DWIE CZYNNOŚCI (wypisuje na ekran i zwraca wartość playerPoints)
+//    public static Integer getPlayerScore(HashMap<Integer,Integer> mapAnswer){
+//        Integer playerPoints = 0;
+//        for(Map.Entry<Integer, Integer> number : mapAnswer.entrySet()){
+//            if(number.getKey().equals(number.getValue())){
+//                playerPoints += 3;
+//            } else {
+//                playerPoints -= 1;
+//            }
+//        }
+//        return playerPoints;
+//    }
+
+    public static int[] oneRoundScore(Integer numberOfPlayers, ArrayList<String> arrayNickList, Integer numberOfRounds){
+
+        int[] playersScore = new int[numberOfPlayers];
+        HashMap<String, Integer> questionsBase = questionsBase();
+
+        for(int round = 0; round < numberOfRounds; round++){
+            System.out.println("***** ROUND NUMBER " + (round+1) + " *****");
+
+            for (int i = 0; i < numberOfPlayers; i++) {
+                System.out.println("Now the player " + arrayNickList.get(i) + " answer the question:");
+                HashMap<Integer, Integer> numberOfAnswer = getNumberOfAnswer(questionsBase);
+                Integer playerScore = printInfAboutAnswer(numberOfAnswer);
+                playersScore[i] += playerScore;
+                System.out.println(arrayNickList.get(i) + " score: " + playersScore[i]);
+                System.out.println();
+            }
+        }
+        return playersScore;
+    }
+
+    public static void printScoreAfterOneRound(int[] playersScore, ArrayList<String> arrayNickList){
+        for(int i = 0; i < playersScore.length; i++){
+            System.out.print("Score player " + arrayNickList.get(i) + " : ");
+            System.out.println(playersScore[i]);
+        }
+    }
+    public static void printResultOfTheGame(int[] playerScore, ArrayList<String> arrayNickList){
+        int bestResult = Integer.MIN_VALUE;
+        for(int i = 0; i < playerScore.length; i++){
+            if(playerScore[i] > bestResult){
+                bestResult = playerScore[i];
+            }
+        }
+        for(int j = 0; j < playerScore.length; j++){
+            if(playerScore[j] == bestResult){
+                System.out.println("Congratulation! " + arrayNickList.get(j) + " win this game!");
             }
         }
     }
