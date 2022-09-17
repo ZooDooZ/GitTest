b49208a4478c459c5ce60b274bc67c6e09927f78
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-09-17 12:08:09 +0200

    Add 2 exercises

--- src/main/java/org/FilipS/App.java
+++ src/main/java/org/FilipS/App.java
@@ -1,8 +1,30 @@
 package org.FilipS;
 
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class App {
     public static void main( String[] args ) {
+
+        // 1. Wprowadź swoje imię do zmiennej typu String. Wypisz w konsoli „Witaj, <imię>” wykorzystując funkcję System.out.println().
+//        String name = "Filip";
+//        System.out.println("Witaj, " + name);
+
+
+        // 2. Wprowadź dowolną wartość z kilkoma cyframi po przecinku do zmiennej typu double.
+        //Wyświetl podaną wartość z dokładnością do dwóch miejsc po przecinku. Uzyj funkcji System.out.printf().
+        //Wyświetl podaną wartość z dokładnością do 4 miejsc po przecinku. Uzyj funkcji System.out.println() oraz String.format().
+
+//        double number = 5.5567899;
+//
+//        System.out.printf("%.2f\n", number);
+//        System.out.printf("%.4f\n", number);
+//
+//        String formatted = String.format("%.4f",number);
+//        System.out.println(formatted);
+
 
     }
 }
